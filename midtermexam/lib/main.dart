cb1d91e5f3a7779fd2e44b2678be99ba6ba04ea8
Angelo <angelolouis.baricuatro.20@usjr.edu.ph>
2022-11-13 18:35:53 +0800

    Midterm Exam To Do List

---
+++ midtermexam/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:test2/listapp.dart';
+import 'package:flutter/material.dart';
+
+// class Task {
+//   Task({required this.task});
+//   final String task;
+//   // bool status;
+// }
+
+void main()=>runApp(const ToDoList());
+
+class ToDoList extends StatelessWidget{
+  const ToDoList({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Midterm Exam',
+
+      home: ToDoApp(),
+    );
+  }
+}
